61eb463bf1dfefab017f7aec4a0e4725a95a772e
Sammy Baek <whitesw01@gmail.com>
2023-10-05 11:41:02 +0900

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,7 @@ import '../styles/global.css';
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+export function reportWebVitals(metric) {
+  console.log(metric);
+}
